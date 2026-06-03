803b55488cb1ee1c49e1ecf6ebb5f0518ed5b44e
aisyais <nur.aisyah.05119@gmail.com>
2026-06-03 17:08:48 +0800

    update lagi

--- siapparkir/frontend/src/pages/internal/admin/PreviewPDF.jsx
+++ siapparkir/frontend/src/pages/internal/admin/PreviewPDF.jsx
@@ -19,7 +19,9 @@ export default function PreviewPDF() {
     setRefId("REKAP-" + Math.floor(100000 + Math.random() * 900000));
   }, []);
 
-  const totalDerek = data.filter(item => item.tindakan === "DEREK PAKSA").length;
+  const totalDerek = data.filter(item =>
+  item.tindakan?.toUpperCase().includes('DEREK')
+).length;
 
   return (
     <div className="bg-gray-100 min-h-screen py-10 antialiased print:bg-white print:p-0">
